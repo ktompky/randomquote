b1c406279019c990c4e999e7c08a969caa4a01ba
ktompky <k.tompkins005@gmail.com>
2019-02-12 11:51:03 -0600

    Adding option to get another quote

--- quote.py
+++ quote.py
@@ -1,15 +1,28 @@
 import requests
 import json
 
-
-r = requests.get('http://quotesondesign.com/api/3.0/api-3.0.json')
-data = r.json()
+user_choice = input("Do you want to get a quote? 'y' or 'n' ")
 
 
-author = data.get('author')
-quote = data.get('quote')
+def getquote():
 
-print(quote, "\n  \t\t\t- by", author)
+    r = requests.get('http://quotesondesign.com/api/3.0/api-3.0.json')
+    data = r.json()
+
+    author = data.get('author')
+    quote = data.get('quote')
+
+    print(quote, "\n  \t\t\t- by", author)
+
+
+while user_choice == 'y':
+    getquote()
+    user_choice = input("Would you like another quote? 'y' or 'n' ")
+if user_choice == 'n':
+    print("Okayyyyyy.")
+
+
+
 
 
 
