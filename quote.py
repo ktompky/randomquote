6b9293bd08f855562b18e3051caeaab7a4ce0c73
Kelly <k.tompkins005@gmail.com>
2023-05-01 14:31:00 -0500

    adding new font

--- quote.py
+++ quote.py
@@ -1,6 +1,9 @@
 import requests
 import json
 import random
+from PIL import Image, ImageFont, ImageDraw
+
+
 
 def getquote():
 
@@ -10,10 +13,22 @@ def getquote():
     quote = random.choice(data)
     print(quote['text'])
     print(quote['author'])
+    return quote['text'] + " \n" + quote['author']
 
 
-getquote()
+final_quote = getquote()
 
+# open image
+my_image = Image.open('wide_image.jpg')
+title_font = ImageFont.truetype('Raleway/static/Raleway-Bold.ttf', 100)
+# draw image object
+image_editable = ImageDraw.Draw(my_image)
+
+# add text to image
+image_editable.text((215,515), final_quote, (0, 0, 0), font=title_font)
+
+# save image
+my_image.save("result.jpg")
 
 
 
