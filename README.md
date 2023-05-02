0bcc698bcd8ccd5caa070db1580c8a2119aa9663
Kelly <k.tompkins005@gmail.com>
2023-05-02 11:51:31 -0500

    put draw actions into a function

---
+++ README.md
@@ -0,0 +1,22 @@
+## Random Quote Generator 
+---
+### What it does v1
+- Using the requests library, gets a random quote from an api
+- Saves the quote in a variable
+- With the pillow module, draws the quote on an image
+
+
+### What's required v1
+- Pillow module
+- Requests library
+- An api to query for a random quote - A Truetype font type 
+- An image to draw on
+
+
+
+### V2 
+- gives user option to pull random quote or to pull from list of fortunes
+
+
+### v3
+- made into a discord bot
--- quote.py
+++ quote.py
@@ -16,21 +16,22 @@ def getquote():
     return quote['text'] + " \n" + quote['author']
 
 
-final_quote = getquote()
+def finalImage():
+    final_quote = getquote()
 
-# open image
-my_image = Image.open('wide_image.jpg')
-title_font = ImageFont.truetype('Raleway/static/Raleway-Bold.ttf', 100)
-# draw image object
-image_editable = ImageDraw.Draw(my_image)
+    # open image
+    my_image = Image.open('wide_image.jpg')
+    title_font = ImageFont.truetype('Raleway/static/Raleway-Bold.ttf', 100)
+    # draw image object
+    image_editable = ImageDraw.Draw(my_image)
 
-# add text to image
-image_editable.text((215,515), final_quote, (0, 0, 0), font=title_font)
+    # add text to image
+    image_editable.text((215,515), final_quote, (0, 0, 0), font=title_font)
 
-# save image
-my_image.save("result.jpg")
+    # save image
+    my_image.save("result.jpg")
 
-
+finalImage()
 
 
 
